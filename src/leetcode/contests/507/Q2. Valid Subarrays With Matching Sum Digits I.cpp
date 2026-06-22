b9e714c18032a25329b590ac41d5e49a1c43320a
phucnp <phucnguyenphi123@gmail.com>
2026-06-22 10:04:27 +0700

    [update] completed 3 of 4 problems in leetcode contest 507 (key point: shortest path in graph of state node)

---
+++ src/leetcode/contests/507/Q2. Valid Subarrays With Matching Sum Digits I.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+    int passwordStrength(string password) {
+        priority_queue<char> pq;
+        pq.push();
+        pq.top();
+        return point;
+    }
+};
